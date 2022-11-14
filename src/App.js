57959df8cd51dd3eac8d59bac030d3e597700300
Adriano <adrianosantosr1@gmail.com>
2022-11-13 23:42:15 -0300

    criando dados estáticos e estilizando primeira parte

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BsBoxArrowRight } from "react-icons/bs";
+import "./style.css";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <div className="containerItems">
+        <input type="text" placeholder="Digite o CEP" />
+        <button className="buttonSearch">
+          <BsBoxArrowRight size={25} color="#eee" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 74 578 027</h2>
+
+        <span> Rua teste algum</span>
+        <span>Complemento: algum</span>
+        <span>Planaltina</span>
+        <span>Brasilia-DF</span>
+      </main>
+    </div>
+  );
 }
 
 export default App;
