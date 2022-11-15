18928a086c2b968d8588f60ab7107e0e142af0d1
Adriano <adrianosantosr1@gmail.com>
2022-11-14 22:53:16 -0300

    consumindo api e adicionando component de resposta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import { BsBoxArrowRight } from "react-icons/bs";
+import { useState } from "react";
 import "./style.css";
+import api from "./services/api";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Digite novamente");
+      return;
+    }
+
+    try {
+      const { data } = await api.get(`${input}/json`);
+      setCep(data);
+      setInput("");
+    } catch {
+      alert("ops! CEP não encontrado");
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
+
       <div className="containerItems">
-        <input type="text" placeholder="Digite o CEP" />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite o CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
           <BsBoxArrowRight size={25} color="#eee" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 74 578 027</h2>
+      {Object.keys(cep).length > 1 && (
+        <main className="main">
+          <h2>CEP:{cep.cep}</h2>
 
-        <span> Rua teste algum</span>
-        <span>Complemento: algum</span>
-        <span>Planaltina</span>
-        <span>Brasilia-DF</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade}-{cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
